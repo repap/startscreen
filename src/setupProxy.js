e6e440592e2a1e3bfe78ad905122b0b08ab4d118
Dennis Penning <penning.dennis@googlemail.com>
2020-04-25 06:53:57 +0200

    Add api proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.API_URL,
+      changeOrigin: true,
+    })
+  );
+};
